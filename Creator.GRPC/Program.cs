6e7ab627be157cecbcd94ee1b39b69e0aae9382f
unknown <felixyangyicheng@gmail.com>
2025-07-04 11:32:12 +0200

    repository

--- Creator.GRPC/Program.cs
+++ Creator.GRPC/Program.cs
@@ -1,21 +1,70 @@
 ﻿
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using Insertion;
+using Newtonsoft.Json;
+using System.Buffers.Text;
+using System.Text;
+using System.Xml.Linq;
 using Insertion;
 
 namespace Creator.GRPC
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static InsertRequest logModel = new();
+        static string baseUrl = "http://localhost:5000/";
+        static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddGrpcClient<InsertService.InsertServiceClient>(options =>
+            Console.WriteLine("running on " + baseUrl);
+            while (true)
             {
-                // 配置服务地址
-                options.Address = new Uri("http://localhost:5000");
-            })
-           // .AddInterceptor<ClientLoggerInterceptor>()  // 添加客户端拦截器
-            .EnableCallContextPropagation();           // 启用调用上下文传播
+                try
+                {
+                    logModel = Applog.getlogData();
+                    if (logModel != null)
+                    {
+
+                        using var channel = GrpcChannel.ForAddress(baseUrl);
+                        var client = new InsertService.InsertServiceClient(channel);
+
+                      
+                        var reply = await client.InsertEntryAsync(logModel);
+                        Console.WriteLine("Status: " + reply.BodyMessage+ " " +reply.Code+" " + reply.Detail);
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("exception" + " " + e.Message);
+                }
+
+                Thread.Sleep(4000);
+            }
+        }
+    }
+
+    static class Applog
+    {
+        public static InsertRequest getlogData()
+        {
+            InsertRequest objdata = new InsertRequest()
+            {
+                Detail = "Operation-Code~" + Utilities.RandomNumber(1, 1000),
+                Value = Utilities.RandomNumber(1, 100),
+                DateOperation = (DateTime.UtcNow).ToTimestamp(),
+            };
+
+            return objdata;
+        }
+    }
+
+    static class Utilities
+    {
+        public static int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
         }
     }
 }
