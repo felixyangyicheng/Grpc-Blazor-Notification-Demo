529641990890e604fb7889d5b8cae9cda316a54e
unknown <felixyangyicheng@gmail.com>
2025-08-21 12:14:47 +0200

    service

---
+++ Realtime_D3.GRPC.Blazor/Realtime_D3.GRPC.Blazor/Services/GrpcChannelProvider.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+
+namespace Realtime_D3.GRPC.Blazor.Services
+{
+    public class GrpcChannelProvider 
+    {
+
+    }
+}
---
+++ Realtime_D3.GRPC.Blazor/Realtime_D3.GRPC.Blazor/Services/IGrpcChannelProvider.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Net.Client;
+
+namespace Realtime_D3.GRPC.Blazor.Services
+{
+    public interface IGrpcChannelService
+    {
+        GrpcChannel GetEntryChannel();
+        GrpcChannel GetVaultChannel();
+    }
+}
