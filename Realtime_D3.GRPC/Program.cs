1f45016af9b12ec0cfbe96987a9494b77bdf6513
felixyangyicheng <felix.yang.yicheng@gmail.com>
2026-03-08 09:54:43 +0100

    .EnableGrpcWeb()

--- Realtime_D3.GRPC/Program.cs
+++ Realtime_D3.GRPC/Program.cs
@@ -5,34 +5,33 @@ using RealTime_D3.GRPC.Repository;
 using Serilog;
 using System.IO.Compression;
 
-namespace Realtime_D3.GRPC
+namespace Realtime_D3.GRPC;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+        var connString = builder.Configuration.GetConnectionString("postgresql");
+        // Add services to the container.
+        builder.Services.AddGrpc(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var connString = builder.Configuration.GetConnectionString("postgresql");
-            // Add services to the container.
-            builder.Services.AddGrpc(options =>
-            {
-                options.ResponseCompressionLevel = CompressionLevel.Optimal;
-                options.ResponseCompressionAlgorithm = "gzip";
-            });
-            builder.Services.AddDbContext<GrpcDbContext>(options => options.UseNpgsql(connString));
-            builder.Host.UseSerilog((ctx, lc) =>
-                lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+            options.ResponseCompressionLevel = CompressionLevel.Optimal;
+            options.ResponseCompressionAlgorithm = "gzip";
+        });
+        builder.Services.AddDbContext<GrpcDbContext>(options => options.UseNpgsql(connString));
+        builder.Host.UseSerilog((ctx, lc) =>
+            lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 
-            builder.Services.AddScoped<ITbllogRepository, TbllogRepository>();
+        builder.Services.AddScoped<ITbllogRepository, TbllogRepository>();
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            app.MapGrpcService<NotificationerService>();
-            app.MapGrpcService<InsertionService>();
-            app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+        // Configure the HTTP request pipeline.
+        app.MapGrpcService<NotificationerService>().EnableGrpcWeb();
+        app.MapGrpcService<InsertionService>().EnableGrpcWeb();
+        app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
-            app.Run();
-        }
+        app.Run();
     }
 }
